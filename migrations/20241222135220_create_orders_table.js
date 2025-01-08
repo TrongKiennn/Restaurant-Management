82ecb69509549098a5c59133f990c7d0b62baab9
Manh Trong Kien <manhtrongkien1901@gmail.com>
2025-01-08 19:45:28 +0700

    Finish for customer function

--- migrations/20241222135220_create_orders_table.js
+++ migrations/20241222135220_create_orders_table.js
@@ -3,19 +3,24 @@
  * @returns { Promise<void> }
  */
 exports.up = async function(knex) {
-    await knex.raw(
-		`CREATE TABLE IF NOT EXISTS orders (
-            order_id SERIAL PRIMARY KEY, 
-            user_id INT NOT NULL, 
-            total NUMERIC(18, 2) NOT NULL,
-            status VARCHAR(50),
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
-            canceled_at TIMESTAMP,
-            completed_at TIMESTAMP,
-            status_payment VARCHAR(50),
-            FOREIGN KEY (user_id) REFERENCES users(id)
-        );`
+  await knex.raw(
+  `CREATE TABLE IF NOT EXISTS orders (
+    order_id SERIAL PRIMARY KEY, 
+    user_id INT NOT NULL, 
+    total NUMERIC(18, 2) NOT NULL,
+    status TEXT CHECK (status IN ('Pending', 'Completed', 'Delivered', 'Confirmed', 'Canceled', 'In Transit')) DEFAULT 'Pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
+    canceled_at TIMESTAMP,
+    completed_at TIMESTAMP,
+    status_payment TEXT CHECK (status_payment IN ('Paid', 'Unpaid')) DEFAULT 'Unpaid',
+    order_code TEXT,
+    address TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+`
 	);
+
+  
 };
 
 exports.down = async function (knex) {
--- migrations/20241222135326_create_detail_orders_table.js
+++ migrations/20241222135326_create_detail_orders_table.js
@@ -13,9 +13,67 @@ exports.up = async function(knex) {
             FOREIGN KEY (product_id) REFERENCES products(product_id)
         );`
 	);
+
+  await knex.raw(`
+    CREATE OR REPLACE FUNCTION update_order_total()
+    RETURNS TRIGGER AS $$
+    BEGIN
+        UPDATE orders
+        SET total = (
+            SELECT SUM(detail_orders.quantity * products.price)
+            FROM detail_orders 
+            JOIN products ON detail_orders.product_id = products.product_id
+            WHERE detail_orders.order_id = NEW.order_id
+        )
+        WHERE orders.order_id = NEW.order_id;
+
+        RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+  `);
+
+  await knex.raw(`
+    CREATE TRIGGER trg_update_order_total
+    AFTER INSERT OR UPDATE OR DELETE
+    ON detail_orders
+    FOR EACH ROW
+    EXECUTE FUNCTION update_order_total();
+  `);
+
+  await knex.raw(`
+    CREATE SEQUENCE order_code_seq START 1;
+  `);
+
+  await knex.raw(`
+    CREATE OR REPLACE FUNCTION generate_order_code()
+    RETURNS TRIGGER AS $$
+    BEGIN
+        NEW.order_code := 'DH' || LPAD(nextval('order_code_seq')::TEXT, 3, '0');
+        RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+  `);
+
+  await knex.raw(`
+    CREATE TRIGGER trg_generate_order_code
+    BEFORE INSERT ON orders
+    FOR EACH ROW
+    EXECUTE FUNCTION generate_order_code();
+  `);
+
+  await knex.raw(`
+    SELECT setval('products_product_id_seq', (SELECT MAX(product_id) FROM products));
+  `);
 };
 
 exports.down = async function (knex) {
 	await knex.raw(`DROP TABLE IF EXISTS detail_orders;`);
+  await knex.raw(`DROP TRIGGER IF EXISTS trg_update_order_total ON detail_orders`);
+  await knex.raw(`DROP FUNCTION IF EXISTS update_order_total`);
+
+  await knex.raw(`DROP TRIGGER IF EXISTS trg_generate_order_code ON orders`);
+  await knex.raw(`DROP FUNCTION IF EXISTS generate_order_code`);
+
+  await knex.raw(`DROP SEQUENCE IF EXISTS order_code_seq`);
 };
 
